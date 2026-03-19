313bdcc406539e4295e0df63611810cb74eb4f44
20lucia06 <lucia.zunisan@alum.uca.es>
2026-03-19 13:10:14 +0100

    se supone que ya estan las aestructuras si hay algun problema contactenme

--- Escape-Room/configuracion.h
+++ Escape-Room/configuracion.h
@@ -9,8 +9,8 @@ typedef struct
     char nomb_jugador[21];
     char jugador[11];
     char contrasena[9]; //HE CAMBIADO LA Ñ PORQUE SINO EL PROGRAMA NO FUNCIONA PARA QUE USEIS SIEMPRE SIN LA Ñ
-    char id_obj[5][4];
-} Jugadores;
+    char id_obj[5][5];
+} jugadores;
 
 
 typedef struct
@@ -19,17 +19,25 @@ typedef struct
     char nombre_sala[31];
     char tipo[8]; /* INICIAL, NORMAL O SALIDA */
     char descrip[151];
-} Salas;
+} salas;
 
 typedef struct
 {
-    int id_puzles;
+    char id_puzles[4]; // es por ejemplo P01,P02,...
     char nomb_puz[16];
     int id_sala;
     int tipo; /* Código es 0 o Palabra es 1 */
     char descrip[151];
-    char sol[51];
-} Puzles;
+    char sol[51]; /* solución */
+} puzles;
+
+typedef struct
+{
+    char id_obj[5];
+    char nomb_obj[16];  //16 pa el espacio del \0
+    char descrip[51];
+    int id_sala; /* si es -1 esta en el inventario */
+} objetos;
 
 typedef struct
 {
@@ -38,25 +46,15 @@ typedef struct
     int id_destino;
     int estado; /* Activo es 0 y Bloqueado es 1 */
     int cond; /* booleano 0 si no aplica, id_obj o id_puzle */
-    Puzles id_puzles;
-    Objetos id_objeto;
-} Conexiones;
+} conexiones;
 
 typedef struct
 {
-    char id_obj[5];
-    char nomb_obj[15];
-    char descrip[51];
-    Salas id_salas; /* si este campo esta vacío se entiende que esta en el inventario */
-} Objetos;
-
-typedef struct
-{
-    /* Id_jugador
-    id_sala
-    id_obj, localización
-    id_conexion, Estado
-    puzle: id_puzle, resuelto/pendiente*/
-} Partida; /* aqui se unen todas las estructuras */
+    jugadores jugador[20];
+    salas sala[30];
+    puzles puzle[30];
+    objetos objeto[20];
+    conexiones conexion[20];
+} partida; /* aqui se unen todas las estructuras */
 
 #endif // CONFIGURACION
