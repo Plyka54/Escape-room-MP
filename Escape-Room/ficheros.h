bc0450ce7e53841336d02e30780269eb0fde76fe
angeelamarc <an.escribanomarcos@alum.uca.es>
2026-03-25 18:52:36 +0100

    jisjisjis

--- Escape-Room/ficheros.h
+++ Escape-Room/ficheros.h
@@ -3,7 +3,7 @@
 
 #include "configuracion.h"
 
-
+void carga(partida *p,int *total_leidos);
 int comprobar_usuario(partida p,char user[11], int total_leidos, int *u);
 int comprobar_clave(partida p, char *clave, int u);
 
