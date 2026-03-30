c97c3e5f126606203cc85e00705126d9a8e63a6f
Plyka54 <laura.viaalba@alum.uca.es>
2026-03-30 17:52:09 +0200

    primer puzle

--- Escape-Room/puzles.c
+++ Escape-Room/puzles.c
@@ -1,3 +1,39 @@
 #include <stdio.h>
+#include <windows.h>
 #include "puzles.h"
 
+void puzle_morse(partida *p){
+
+    char respuesta;
+
+
+    printf("Me acerque al ordenador, parece que hay algo en la pantalla.\n");
+    printf("Hay un audio en el centro, quieres reproducirlo? (s/n)\n");
+
+    scanf(" %c", &respuesta);
+
+    if (respuesta=='s'){
+
+
+        Beep(750, 300);
+        Beep(750, 300);     //U     ..-
+        Beep(750, 600);
+
+        system("pause");
+
+        Beep(750, 600);
+        Beep(750, 300);
+        Beep(750, 600);     //C     -.-.
+        Beep(750, 300);
+
+        system("pause");
+
+        Beep(750, 300);
+        Beep(750, 600);     //A     .-
+
+    }
+
+    printf("Eso es todo al parecer... Parece codigo morse.\n");
+    printf("Debe de haber algo que me ayude a traducirlo...\n\n\n");
+
+}
--- Escape-Room/puzles.h
+++ Escape-Room/puzles.h
@@ -1,6 +1,7 @@
 #ifndef PUZLES
 #define PUZLES
+#include "configuracion.h"
 
-
+void puzle_morse(partida *p);
 
 #endif // PUZLES
