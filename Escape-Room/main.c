61da0b1e72be7745b4188837ddfaa0adc30ffbe0
angeelamarc <an.escribanomarcos@alum.uca.es>
2026-04-10 17:03:30 +0200

    -

--- Escape-Room/main.c
+++ Escape-Room/main.c
@@ -19,7 +19,6 @@ int main(){
     //CARGAMOS LOS FICHEROS
     int total_usuarios;
     carga(&p,&total_usuarios);
-    printf("%d\n",p.puzle[4].id_sala);
 
 // LOGIN DE USUARIO O REGISTRO -----------------------------------------------------------
     int u;
